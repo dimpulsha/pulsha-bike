71d7845224bdd5e35be818b0d69b07a9d0581742
dimpulsha <dimpulsha@gmail.com>
2021-12-12 00:52:26 +0500

    скрипт открытия меню и форматирование кода

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,25 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var pageHeader = document.querySelector('.page');
+var headerToggle = document.querySelector('.nav__menu-toggle');
+// var menuItems = document.querySelectorAll('.menu-list__link');
 
-pageHeader.classList.remove('page-header--nojs');
+pageHeader.classList.remove('page--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+headerToggle.addEventListener('click', function (e) {
+  e.preventDefault();
+  pageHeader.classList.toggle('page--menu-open');
 });
+
+// if (menuItems) {
+//   for (let i = 0; i < menuItems.length; i++) {
+//     let menuItem = menuItems[i];
+//     let sectionId = menuItem.getAttribute('href').substring(1);
+//     menuItem.addEventListener('click', function (evt) {
+//       evt.preventDefault();
+//       pageHeader.classList.remove('page--menu-open');
+//       document.getElementById(sectionId).scrollIntoView({
+//         block: "start", behavior: "smooth"
+//       })
+//     });
+//   }
+// }
