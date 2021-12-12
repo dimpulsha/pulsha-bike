06628e890969a7a6470360731311cf9ce876cab6
dimpulsha <dimpulsha@gmail.com>
2021-12-12 19:11:58 +0500

    меню и первый блок. раздел с фактами

--- build/js/script.js
+++ build/js/script.js
@@ -2,6 +2,10 @@
 var pageHeader = document.querySelector('.page');
 var headerToggle = document.querySelector('.nav__menu-toggle');
 var menuItems = document.querySelectorAll('.menu-list__link');
+var nameInput = document.querySelector('.contact-form__input--name');
+var telInput = document.querySelector('.contact-form__input--tel');
+var submitForm = document.querySelector('.contact-form__button');
+
 
 pageHeader.classList.remove('page--nojs');
 
@@ -14,17 +18,46 @@ if (menuItems) {
   for (let i = 0; i < menuItems.length; i++) {
     let menuItem = menuItems[i];
     let sectionId = menuItem.getAttribute('href').substring(1);
-    // console.log(sectionId);
     menuItem.addEventListener('click', function (evt) {
       evt.preventDefault();
-
-      // let idValue = menuItem.getAttribute('href').substring(1);
       pageHeader.classList.remove('page--menu-open');
       document.getElementById(sectionId).scrollIntoView({
-        block: "center", behavior: "smooth"
-      })
-      // console.log(menuItem)
-      // console.log(idValue);
+        block: "start", behavior: "smooth"
+      });
     });
   }
 }
+
+function checkEmptyName() {
+  if (nameInput.value.length === 0) {
+    nameInput.setCustomValidity('Имя не заполнено');
+    nameInput.classList.add('contact-form__input--error');
+  } else {
+    nameInput.setCustomValidity('');
+  }
+  nameInput.reportValidity();
+}
+
+function checkEmptyTel() {
+  if (telInput.value.length === 0) {
+    telInput.setCustomValidity('Телефон не заполнен');
+    telInput.classList.add('contact-form__input--error');
+  } else {
+    telInput.setCustomValidity('');
+  }
+  nameInput.reportValidity();
+}
+
+nameInput.addEventListener('input', function () {
+  nameInput.classList.remove('contact-form__input--error');
+});
+
+telInput.addEventListener('input', function () {
+  telInput.classList.remove('contact-form__input--error');
+});
+
+submitForm.addEventListener('click', function () {
+  checkEmptyName();
+  checkEmptyTel();
+});
+
--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,11 @@
 'use strict';
 var pageHeader = document.querySelector('.page');
 var headerToggle = document.querySelector('.nav__menu-toggle');
-// var menuItems = document.querySelectorAll('.menu-list__link');
+var menuItems = document.querySelectorAll('.menu-list__link');
+var nameInput = document.querySelector('.contact-form__input--name');
+var telInput = document.querySelector('.contact-form__input--tel');
+var submitForm = document.querySelector('.contact-form__button');
+
 
 pageHeader.classList.remove('page--nojs');
 
@@ -10,16 +14,50 @@ headerToggle.addEventListener('click', function (e) {
   pageHeader.classList.toggle('page--menu-open');
 });
 
-// if (menuItems) {
-//   for (let i = 0; i < menuItems.length; i++) {
-//     let menuItem = menuItems[i];
-//     let sectionId = menuItem.getAttribute('href').substring(1);
-//     menuItem.addEventListener('click', function (evt) {
-//       evt.preventDefault();
-//       pageHeader.classList.remove('page--menu-open');
-//       document.getElementById(sectionId).scrollIntoView({
-//         block: "start", behavior: "smooth"
-//       })
-//     });
-//   }
-// }
+if (menuItems) {
+  for (let i = 0; i < menuItems.length; i++) {
+    let menuItem = menuItems[i];
+    let sectionId = menuItem.getAttribute('href').substring(1);
+    menuItem.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      pageHeader.classList.remove('page--menu-open');
+      document.getElementById(sectionId).scrollIntoView({
+        block: "start", behavior: "smooth"
+      });
+    });
+  }
+}
+
+function checkEmptyName() {
+  if (nameInput.value.length === 0) {
+    nameInput.setCustomValidity('Имя не заполнено');
+    nameInput.classList.add('contact-form__input--error');
+  } else {
+    nameInput.setCustomValidity('');
+  }
+  nameInput.reportValidity();
+}
+
+function checkEmptyTel() {
+  if (telInput.value.length === 0) {
+    telInput.setCustomValidity('Телефон не заполнен');
+    telInput.classList.add('contact-form__input--error');
+  } else {
+    telInput.setCustomValidity('');
+  }
+  nameInput.reportValidity();
+}
+
+nameInput.addEventListener('input', function () {
+  nameInput.classList.remove('contact-form__input--error');
+});
+
+telInput.addEventListener('input', function () {
+  telInput.classList.remove('contact-form__input--error');
+});
+
+submitForm.addEventListener('click', function () {
+  checkEmptyName();
+  checkEmptyTel();
+});
+
