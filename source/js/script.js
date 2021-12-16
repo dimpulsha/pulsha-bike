05b58aa8f3ffbd3277ca01b7a1a8688374eaa5d1
dimpulsha <dimpulsha@gmail.com>
2021-12-16 13:22:19 +0500

    правки LocalStorage

--- source/js/script.js
+++ source/js/script.js
@@ -60,3 +60,19 @@ submitForm.addEventListener('click', function () {
   checkEmptyName();
   checkEmptyTel();
 });
+
+if (window.localStorage) {
+  var elements = document.querySelectorAll('[name]');
+
+  for (var i = 0, length = elements.length; i < length; i++) {
+    (function (element) {
+      var name = element.getAttribute('name');
+
+      element.value = localStorage.getItem(name) || element.value;
+
+      element.onkeyup = function () {
+        localStorage.setItem(name, element.value);
+      };
+    })(elements[i]);
+  }
+}
